9560404481014597c12a1a7b2621b3d8304fcf24
unknown <devops@simplifiedschooling.in>
2024-07-15 16:02:17 +0530

    new changes

--- src/app/manufacturer/profile/profile.component.ts
+++ src/app/manufacturer/profile/profile.component.ts
@@ -20,22 +20,30 @@ export class ProfileComponent {
   mgfRegistrationForm:any= FormGroup;
   submitted: boolean = false;
   userProfile: any
+  getResisterData:any
+
+  // btn flag
+  isDataSaved = false;  // Flag to track if data is saved
+  submitFlag = false;
+  isUpdateBtn=false;
+  isEditFlag=false
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
-
 
   }
 
 
 
   ngOnInit(): void {
-    this.userProfile = JSON.parse(localStorage.getItem("currentUser")!);
-    this.InisilizeValidation()
-    this.getProfileData()
+    this.userProfile = JSON.parse(localStorage.getItem("currentUser")!);    
+    this.InisilizeValidation()     
+    this.getSavedProfileData()   
+    this.disbledFeilds()
   }
 
   InisilizeValidation() {
     this.mgfRegistrationForm = this.fb.group({
+      fullName: ['', Validators.required],
       companyName: ['', Validators.required],
       address: ['', Validators.required],
       state: ['', Validators.required],
@@ -66,22 +74,57 @@ export class ProfileComponent {
     return this.mgfRegistrationForm.controls;
   }
 
+  // get resisterd data
+  getResistereduserData(){
+    this.authService.get(`users/registered-user/${this.userProfile.email}`).subscribe(res=>{
+      this.getResisterData=res
+      this.mgfRegistrationForm.patchValue({
+        companyName: this.getResisterData.companyName,
+        mobNumber:this.getResisterData.mobileNumber,
+        email:this.getResisterData.email,
+        fullName:this.getResisterData.fullName
+      });
 
-  getProfileData() {
+    },
+    error=>{
+
+    }
+  )
+  }
+
+  // disbled some resistered feilds
+  disbledFeilds(){
+    this.mgfRegistrationForm.get('fullName')?.disable();
+    this.mgfRegistrationForm.get('email')?.disable();
+    this.mgfRegistrationForm.get('mobNumber')?.disable();
+    this.mgfRegistrationForm.get('companyName')?.disable();
+}
+
+
+  getSavedProfileData() {
     this.authService.get(`manufacturers/${this.userProfile.email}`).subscribe((res: any) => {
       if (res) {
         const allData = res
         this.mgfRegistrationForm.patchValue(allData)
+        this.mgfRegistrationForm.disable()
+        this.isDataSaved=true
+        this.isEditFlag=true;
       }
       else {
-        this.mgfRegistrationForm.patchValue(this.userProfile)
+      
       }
-    })
+    }
+  ,error=>{
+    if(error.error.message == "Manufaturer not found"){
+    this.getResistereduserData()
+  }
+  })
   }
 
 
 
   onSubmit(Type: any): void {
+    this.mgfRegistrationForm
     this.submitted = true;
     if (this.mgfRegistrationForm.invalid) {
       return;
@@ -99,8 +142,14 @@ export class ProfileComponent {
   // save and update
 
   SaveProfileData() {
-    this.authService.post('manufacturers', this.mgfRegistrationForm.value).subscribe((res: any) => {
+    const formData=this.mgfRegistrationForm.getRawValue();
+    
+    this.authService.post('manufacturers',formData ).subscribe((res: any) => {
       console.log(res)
+      if(res){
+        this.mgfRegistrationForm.disable()   
+        this.isEditFlag=true;     
+      }
     },
       error => {
 
@@ -110,13 +159,23 @@ export class ProfileComponent {
   }
 
   UpdateData() {
-   this.authService.patchWithEmail(`manufacturers/${this.userProfile.email}`,this.mgfRegistrationForm.value)
+    const formData=this.mgfRegistrationForm.getRawValue();
+   this.authService.patchWithEmail(`manufacturers/${this.userProfile.email}`,formData)
    .subscribe(res=>{
-  console.log(res)
+     if(res){
+      this.mgfRegistrationForm.disable()
+      this.isUpdateBtn=false; 
+     }
    },
    error=>{
 
    })
+  }
+
+  editUserData(){
+    this.mgfRegistrationForm.enable()
+    this.isUpdateBtn=true 
+    // this.isEditFlag=false
   }
 
 }
