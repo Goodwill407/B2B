a6fb33a69089aef73f83880717e43347e87a4b51
unknown <devops@simplifiedschooling.in>
2024-08-26 18:49:09 +0530

    changes

--- src/app/retailer/dashboard/dashboard.component.ts
+++ src/app/retailer/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { GraphService } from '@core';
+import { BottomSideAdvertiseComponent } from '@core/models/advertisement/bottom-side-advertise/bottom-side-advertise.component';
+import { RightSideAdvertiseComponent } from '@core/models/advertisement/right-side-advertise/right-side-advertise.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
 
 @Component({
@@ -8,7 +10,9 @@ import { NgApexchartsModule } from 'ng-apexcharts';
   standalone: true,
   imports: [
     NgApexchartsModule,
-    CommonModule
+    CommonModule,
+    BottomSideAdvertiseComponent,
+    RightSideAdvertiseComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
@@ -17,6 +21,13 @@ export class DashboardComponent {
   
   public chartOptions: any;
   currentMonth: Date = new Date();
+
+  rightAdImages: string[] = [
+    'https://en.pimg.jp/081/115/951/1/81115951.jpg',
+    'https://en.pimg.jp/087/336/183/1/87336183.jpg'
+  ];
+
+  bottomAdImage: string = 'https://elmanawy.info/demo/gusto/cdn/ads/gusto-ads-banner.png';
 
   constructor(private graphService: GraphService) { }
 
