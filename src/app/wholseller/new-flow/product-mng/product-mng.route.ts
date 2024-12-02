48e67cd72378fb071359368c1b2b97314bcb55d3
unknown <devops@simplifiedschooling.in>
2024-12-02 22:12:42 +0530

    po list in wholesaler

--- src/app/wholseller/new-flow/product-mng/product-mng.route.ts
+++ src/app/wholseller/new-flow/product-mng/product-mng.route.ts
@@ -4,12 +4,14 @@ import { ManufaturesProduct2Component } from "./manufaturer-list2/manufatures-pr
 import { ViewProduct2Component } from "./manufaturer-list2/manufatures-product2/view-product2/view-product2.component";
 import { CartProduct2Component } from "./cart-product2/cart-product2.component";
 import { WishlistProduct2Component } from "./wishlist-product2/wishlist-product2.component";
+import { ViewWishlistproductComponent } from "./manufaturer-list2/manufatures-product2/view-wishlistproduct/view-wishlistproduct.component";
 
 export const ProductMng:Route[]=[
     {path: 'wish-list',component:WishlistProduct2Component},
     {path: 'mnf-list', component: ManufaturerList2Component},
     {path: 'mnf-product', component: ManufaturesProduct2Component},
     {path: 'view-product', component: ViewProduct2Component},
+    {path: 'view-wishlistproduct', component: ViewWishlistproductComponent},
     // {path: 'wishlist-product', component: WishlistProductComponent},
     {path: 'add-to-cart', component: CartProduct2Component},
     // {path: 'ordered-products', component:OrderedProductComponent},
